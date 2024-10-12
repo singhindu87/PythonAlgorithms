b6c457379bb57dbb6d5be40d2cce00add699aa8a
singhindu87 <singh.indu87@gmail.com>
2024-10-12 13:45:00 -0700

    Update Bubble sort.py

--- Sorts/Bubble sort.py
+++ Sorts/Bubble sort.py
@@ -1,1 +1,20 @@
+#bubble sort
+# It compares and sorts the adjacent element if they are in incorrect order
+# In the each pass, between each adjacent element, the max element goes to the end
+# loop 1- i element through all n elements
+# loop 2 - j element will starts from second last element, till ith element, with increments of -1 (travesing from right(max) to left(min)
 
+def bubblesort(arr):
+  n=len(arr)
+
+  for i in range(n):
+    for j in range(n-1,i,-1):
+      if arr[j]<arr[i]:
+        (arr[i],arr[j])=(arr[j],arr[i])
+  return arr
+
+mylist=[30,7,2,15,789,54]
+print(mylist)
+bubblesort(mylist)
+print("Sorted List")
+print(mylist)
